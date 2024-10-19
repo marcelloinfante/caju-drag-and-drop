7fbe6b893e55bb6ed962e457b5cf41e321156279
Marcello Infante <marcelloinfantee@gmail.com>
2024-10-19 09:31:35 -0300

    feat: add theme

---
+++ src/theme/typography.ts
@@ -0,0 +1,107 @@
+export function remToPx(value: string) {
+  return Math.round(parseFloat(value) * 16);
+}
+
+export function pxToRem(value: number) {
+  return `${value / 16}rem`;
+}
+
+export function responsiveFontSizes({
+  sm,
+  md,
+  lg,
+}: {
+  sm: number;
+  md: number;
+  lg: number;
+}) {
+  return {
+    "@media (min-width:600px)": {
+      fontSize: pxToRem(sm),
+    },
+    "@media (min-width:900px)": {
+      fontSize: pxToRem(md),
+    },
+    "@media (min-width:1200px)": {
+      fontSize: pxToRem(lg),
+    },
+  };
+}
+
+interface TypographyStyle {
+  fontWeight: number;
+  lineHeight: number;
+  fontSize: string;
+}
+
+interface Typography {
+  h1: TypographyStyle;
+  h2: TypographyStyle;
+  h3: TypographyStyle;
+  h4: TypographyStyle;
+  h5: TypographyStyle;
+  h6: TypographyStyle;
+  subtitle1: TypographyStyle;
+  subtitle2: TypographyStyle;
+  body1: TypographyStyle;
+  body2: TypographyStyle;
+  caption: TypographyStyle;
+}
+
+export const typography: Typography = {
+  h1: {
+    fontWeight: 800,
+    lineHeight: 80 / 64,
+    fontSize: pxToRem(40),
+  },
+  h2: {
+    fontWeight: 800,
+    lineHeight: 64 / 48,
+    fontSize: pxToRem(32),
+  },
+  h3: {
+    fontWeight: 700,
+    lineHeight: 1.5,
+    fontSize: pxToRem(24),
+  },
+  h4: {
+    fontWeight: 700,
+    lineHeight: 1.5,
+    fontSize: pxToRem(20),
+  },
+  h5: {
+    fontWeight: 700,
+    lineHeight: 1.5,
+    fontSize: pxToRem(18),
+  },
+  h6: {
+    fontWeight: 700,
+    lineHeight: 28 / 18,
+    fontSize: pxToRem(17),
+  },
+  subtitle1: {
+    fontWeight: 600,
+    lineHeight: 1.5,
+    fontSize: pxToRem(16),
+  },
+  subtitle2: {
+    fontWeight: 600,
+    lineHeight: 22 / 14,
+    fontSize: pxToRem(14),
+  },
+  body1: {
+    fontWeight: 400,
+    lineHeight: 1.5,
+    fontSize: pxToRem(16),
+  },
+  body2: {
+    fontWeight: 400,
+    lineHeight: 22 / 14,
+    fontSize: pxToRem(14),
+  },
+  caption: {
+    fontWeight: 400,
+    lineHeight: 1.5,
+    fontSize: pxToRem(12),
+  },
+};
