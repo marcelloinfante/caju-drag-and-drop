374b19438cbf2745104733a24745ad41fe99e799
Marcello Infante <marcelloinfantee@gmail.com>
2024-10-19 19:51:00 -0300

    feat: refactor textfield

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,36 +1,21 @@
-import React, { InputHTMLAttributes } from "react";
-import styled from "styled-components";
+import { InputHTMLAttributes } from "react";
 
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: #ffffff;
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius:8px;
-  :focus {
-    outline: none;
-    border: 1px solid #007c89;
-    box-shadow: inset 0 0 0 1px #007c89;
-  }
-`;
-type Props = {
+import { Input } from "./styles";
+import Typography from "../Typography";
+
+interface TextFieldProps extends InputHTMLAttributes<any> {
   label?: string;
   error?: string;
-} & InputHTMLAttributes<any>;
+}
 
-const TextField = (props: Props) => {
+const TextField = ({ id, label, error, ...props }: TextFieldProps) => {
   return (
     <div>
-      <label htmlFor={props.id}>{props.label}</label>
+      <label htmlFor={id}>{label}</label>
       <Input {...props} />
-      <span style={{fontSize: 12, color: 'red'}}>{props.error}</span>
+      <Typography color="error" variant="caption">
+        {error}
+      </Typography>
     </div>
   );
 };
