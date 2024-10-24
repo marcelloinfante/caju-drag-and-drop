14e88145a8829c204b74eef92c669101ce9539ca
Marcello Infante <marcelloinfantee@gmail.com>
2024-10-24 13:52:43 -0300

    feat: update eslint

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginImport from "eslint-plugin-import";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginPrettier from "eslint-config-prettier";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginImport.flatConfigs.recommended,
+  pluginJsxA11y.flatConfigs.recommended,
+  pluginPrettier,
+  {
+    rules: {
+      "import/named": "off",
+      "import/export": "off",
+      "import/no-named-as-default": 0,
+      "import/no-unresolved": ["error", { ignore: ["^@/", "./", "."] }],
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          paths: ["src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+  },
+];
